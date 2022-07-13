95da624888adf07cf8f737f08334a1a7a42bce59
Artem <sengstoo@gmail.com>
2022-07-13 23:06:57 +0300

    Redux

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Counter from './Counter';
+
+const App = () => {
+  return <Counter/>
+}
+
+export default App;
+
+{/* <Counter
+        counter={getState().value}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+          const value = Math.floor(Math.random() * 10);
+          rnd(value);
+        }}
+      /> */}
